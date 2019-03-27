5d973ca4b37d447f05ebb056bfc5299c2a744730
AntoshkivMaxim <pizza21.41@gmail,com>
2019-03-27 19:33:52 +0200

    Add project files.

---
+++ Framework2/Pages/BasePage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework2.Pages
+{
+    class BasePage
+    {
+        public BasePage(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+    }
+}
---
+++ Framework2/Utils/WebElementExtensions.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework2.Utils
+{
+    static class WebElementExtensions
+    {
+        public static void Setext(this IWebElement element, string text)
+        {
+            element.SendKeys(text);
+        }
+
+        public static void SetDropdownOptionsByText(this IWebElement element, string text)
+        {
+            new SelectElement(element).SelectByText(text);
+        }
+    }
+}
